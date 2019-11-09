80d41e5c2b9f7f4969df011222c2b4ab3bb5bd52
matt4realz <43530617+matt4realz@users.noreply.github.com>
2019-11-09 21:18:09 +0800

    Delete urlParser.py

--- urlParser.py
+++
@@ -1,8 +0,0 @@
-from urllib.parse import urlparse
-
-analysisTxt = open("C:\Users\Matt\Desktop\2202\test.txt", 'r')
-
-for line in analysisTxt:
-    obj = urlparse(analysisTxt)
-    print (obj.netloc)
-    print (obj.path)
